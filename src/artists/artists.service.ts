d132463a70dfc81aae0d84062982dc21ec1c3a71
Dmitry Chigrin <>
2025-12-10 22:56:19 +0500

    feat: add artists functionality

---
+++ src/artists/artists.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Artist } from './interfaces/artist.interface';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class ArtistsService {
+  private artists: Artist[] = [];
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const artist: Artist = {
+      id: randomUUID(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.artists.push(artist);
+    return artist;
+  }
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
+
+  findOne(id: string): Artist {
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+    return artist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artist = this.artists.find((artist) => artist.id === id);
+
+    if (!artist) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    artist.name = updateArtistDto.name;
+    artist.grammy = updateArtistDto.grammy;
+
+    return artist;
+  }
+
+  remove(id: string): void {
+    const index = this.artists.findIndex((artist) => artist.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists.splice(index, 1);
+  }
+}
+
---
+++ src/artists/dto/create-artist.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class CreateArtistDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  grammy: boolean;
+}
+
