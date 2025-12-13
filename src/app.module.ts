72dd517571729c8046e73ac87a4ba889319477e3
Dmitry Chigrin <>
2025-12-14 00:15:01 +0500

    feat: update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
+import { TracksModule } from './tracks/tracks.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { ArtistsService } from './artists/artists.service';
+import { AlbumsService } from './albums/albums.service';
+import { TracksService } from './tracks/tracks.service';
+import { FavoritesService } from './favorites/favorites.service';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ArtistsModule,
+    AlbumsModule,
+    TracksModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(
+    private readonly artistsService: ArtistsService,
+    private readonly albumsService: AlbumsService,
+    private readonly tracksService: TracksService,
+    private readonly favoritesService: FavoritesService,
+  ) {}
+
+  onModuleInit() {
+    // Setup cascade dependencies
+    this.artistsService.setDependencies(
+      this.albumsService,
+      this.tracksService,
+      this.favoritesService,
+    );
+    this.albumsService.setDependencies(
+      this.tracksService,
+      this.favoritesService,
+    );
+    this.tracksService.setDependencies(this.favoritesService);
+  }
+}
